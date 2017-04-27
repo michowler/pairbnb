28fee6bce13f6570837eb4345a3411cf0e9c5e5e
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-27 12:29:34 +0800

    created auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   include Clearance::User
   
-  enum gender: [:undefined, :male, :female]
+  enum gender: [:undefined, :male, :females]
+
+	def self.create_with_auth_and_hash(authentication, auth_hash)
+	     user = User.create!(full_name: auth_hash["name"], email: auth_hash["extra"]["raw_info"]["email"])
+	     user.authentications << (authentication)      return user
+	end
+
+    def fb_token
+      x = self.authentications.where(:provider => :facebook).first
+      return x.token unless x.nil?
+    end
+
 end
