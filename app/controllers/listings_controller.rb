e4466422bfea2ace5f7bf31fd95714f37ba7cd8f
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-27 18:43:31 +0800

    added valid for creating user have to log in

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
 	#before_action :require_login, only: [:new, :create]
+	before_action :logged_in_user, only: [:new, :create]
 	before_action :find_listing, only: [:show, :delete]
 	
 
