5b6a2d32610ad148812dc6e36d13d87a1bbc56e9
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-05 11:38:33 +0800

    done payment without status

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'braintree/new'
-  post 'braintree/checkout'
-
   root to: "pages#index"
 
   resources :listings, controller: "listings"
 
   resources :listings do 
-     resources :reservations, only: [:create]
-   end
+     resources :reservations
+  end
+
   resources :reservations, only: [:destroy]
 
   resources :tags, controller: "tags" do
@@ -26,16 +24,17 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
-  # resources :users, controller: "users", only: [:show] do
-  #   resources :listings,
-  #     controller: "listings",
-  #     only: [:create, :edit, :update, :show]
-  # end
+  resources :users do
+     resources :listings
+  end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  get "/reservations/:rid/transactions/new" => "transactions#new", as: "transactions_new"
+  post '/reservations/:rid/transactions/checkout' => "transactions#checkout", as: "transactions_checkout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
