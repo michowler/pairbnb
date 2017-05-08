52570945208b0c858ab10ced329f7231a6fb8071
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-08 13:19:22 +0800

    checking if params for filtering works

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,9 +10,11 @@ class Listing < ApplicationRecord
 
 	enum place_type: [:guesthouse, :hotel, :apartment, :townhouse, :cabin, :house, :bungalow]
 
-	scope :locations, -> (location) { where("location ilike ?", "#{search}%")}
+	scope :locations, -> (location) { where("location ilike ?", "#{location}%")}
+	scope :prices, -> (price) {where ("price ilike ?", "#{price}")}
 
 	def self.search(search)
+	  #Listing.where('location LIKE :search OR price LIKE :search OR console LIKE :search', search: "%#{search}%")	
 	  #where("title LIKE ? OR tags LIKE ? OR location LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
  	  where("location ilike ?", "%#{search}%")
 	end
