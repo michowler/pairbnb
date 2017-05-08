b11105db85251df739918cbdef575f4995f0d817
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-08 11:43:00 +0800

    fixing error for local host domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pairbnb_#{Rails.env}"
+  config.action_mailer.default_url_options = { :host => "http://www.imingle.herokuapp.com" }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
